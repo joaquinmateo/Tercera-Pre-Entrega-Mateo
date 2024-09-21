56d8fdfbbda21cd35868a53bca1cbd9a6e176c49
Joaquin Mateo <joaquinmateo2003@gmail.com>
2024-09-21 13:37:29 -0300

    Empezamos con la Tercera Pre-entrega

---
+++ app_tercera_preentrega_mateo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppTerceraPreentregaMateoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_tercera_preentrega_mateo'
